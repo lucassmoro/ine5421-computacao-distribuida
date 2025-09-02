ebc49c3ca8a5e9756596ec03f0baeca0fb658948
lucassmoro <lucas.sm.souza@gmail.com>
2025-09-02 20:43:59 -0300

    exercicio1 pt.2

--- ex1-server.c
+++ ex1-server.c
@@ -11,22 +11,27 @@ int main(){
     //socklen_t 
     int server_len, client_len;
     struct sockaddr_in server_addr, client_addr;
-    char msg;
-
+    char msg[100];
     serversocket = socket(AF_INET, SOCK_STREAM, 0);
     server_addr.sin_family = AF_INET;
-    server_addr.sin_addr.s_addr = inet_addr("127.0.0.1");
+    server_addr.sin_addr.s_addr = inet_addr("192.168.171.165");
     server_addr.sin_port = htons(1100);
 
     server_len = sizeof(server_addr);
-    bind(serversocket, (struct sockaddr *)&server_addr, server_len);
-    listen(serversocket, 1);
+    if (bind(serversocket, (struct sockaddr *)&server_addr, server_len) == -1) {
+        perror("bind");
+        exit(1);
+    }
+    listen(serversocket, 5);
         
     client_len = sizeof(client_addr);
-    clientsocket = accept(serversocket, (struct sockaddr*)&client_addr, &client_len);
-    read(clientsocket, &msg, 1);
-    printf("recebendo %c e enviando de volta", msg);
-    write(clientsocket, &msg, 1);
+    // aceita conexao com um socket so e fica recebendo coisa dele
+    clientsocket = accept(serversocket, (struct sockaddr*)&client_addr, &client_len); 
+    while(1){
+        read(clientsocket, &msg, 5);
+        printf("recebendo %s e enviando de volta\n", msg);
+        sleep(1);
+        write(clientsocket, &msg, 5);
+    }
     close(clientsocket);
-    
 }
--- ex1_client.c
+++ ex1_client.c
@@ -5,26 +5,34 @@
 #include <arpa/inet.h>
 #include <unistd.h>
 #include <stdlib.h>
+#include <string.h>
+
 
 int main(){
     int sockfd;
     struct sockaddr_in sockfdaddr;
-    char msg = 'A';
-    int len;
+    char msg[] = "sexo";
     sockfd = socket(AF_INET, SOCK_STREAM, 0);
-
+    int len;
+    char* palavra = msg;
+    
     sockfdaddr.sin_family = AF_INET;
-    sockfdaddr.sin_addr.s_addr = inet_addr("127.0.0.1");
+    sockfdaddr.sin_addr.s_addr = inet_addr("192.168.171.165");
     sockfdaddr.sin_port = htons(1100);
     len = sizeof(sockfdaddr);
+
     int result = connect(sockfd, (struct sockaddr*)&sockfdaddr, len);
-    if (result == -1){
-        perror("fudeu");
-        exit(1);
+        if (result == -1){
+            perror("fudeu");
+            exit(1);
+        }
+    while(1){
+        write(sockfd, palavra, 1);
+        printf("cliente enviando %c\n", *palavra);
+        sleep(1);
+        read(sockfd, palavra, 1);
+        palavra++;
     }
-    write(sockfd, &msg, 1);
-    read(sockfd, &msg, 1);
-    printf("cliente enviando %c", msg);
     close(sockfd);
     exit(0);
 
