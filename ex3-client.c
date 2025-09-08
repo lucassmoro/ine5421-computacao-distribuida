c3487e2673f59f52cc352f32f2a401aa983b50e0
lucassmoro <lucas.sm.souza@gmail.com>
2025-09-08 10:59:47 -0300

    ex3 ta coletando as mensagens e somando os valores e tb a qtd de vezes. tem q fazer a media em vez da soma

--- ex3-client.c
+++ ex3-client.c
@@ -8,20 +8,21 @@
 #include <string.h>
 
 int main(){
-    int sockfd, len;
+    int sockfd;
     struct sockaddr_in sockfdaddr;
     int numero;
 
     sockfdaddr.sin_family = AF_INET;
     sockfdaddr.sin_addr.s_addr = inet_addr("127.0.0.1");
-    sockfdaddr.sin_port = htons(1102);
+    sockfdaddr.sin_port = htons(6000);
     socklen_t len = sizeof(sockfdaddr);
     sockfd = socket(AF_INET, SOCK_DGRAM, 0);
     while(1){
         numero = rand();
-        sendto(sockfd, numero, sizeof(numero), MSG_CONFIRM, (struct sockaddr*)&sockfdaddr, sizeof(sockfdaddr));
-        int n = recvfrom();
+        sendto(sockfd, &numero, sizeof(numero), MSG_CONFIRM, (struct sockaddr*)&sockfdaddr, sizeof(sockfdaddr));
+        //int n = recvfrom();
     }
     close(sockfd);
+    return 0;
     
 }
--- ex3-server.c
+++ ex3-server.c
@@ -6,7 +6,39 @@
 #include <unistd.h>
 #include <stdlib.h>
 #include <string.h>
+#include <time.h>
 
 int main(){
-    int sockfd;
+    int serversocket;
+    struct sockaddr_in serveraddr, clientaddr;
+    socklen_t server_len, client_len;
+    int buffer[2];
+    int recebidos;
+    int qtd = 0;
+    
+    serversocket = socket(AF_INET, SOCK_DGRAM, 0);
+    serveraddr.sin_addr.s_addr = INADDR_ANY;
+    serveraddr.sin_family = AF_INET;
+    serveraddr.sin_port = htons(6000);
+
+    server_len = sizeof(serveraddr);
+    if (bind(serversocket, (struct sockaddr *) &serveraddr, server_len) < 0) {
+        perror("Bind failed"); close(serversocket); exit(1) ;
+    } 
+
+    client_len = sizeof(clientaddr);
+    time_t tempo = time(NULL);
+    time_t tempo_dif;
+    while(1){
+        int n = recvfrom(serversocket, &recebidos, sizeof(recebidos), 0, (struct sockaddr *) &clientaddr, &client_len);
+        buffer[0] += recebidos;
+        qtd++;
+        if (((tempo_dif = time(NULL)) - tempo) > 1) {
+            printf("SERVER: qtd de medicoes coletadas = %d\n", qtd);
+            printf("SERVER: soma das medicoes = %d\n", buffer[0]);
+            tempo = time(NULL);
+        }
+    }
+    close(serversocket);
+    return 0;
 }
